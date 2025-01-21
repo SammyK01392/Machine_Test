eddcf808348e7a238257d6cf8619ce1b47a32b46
SAMEER KARALE <sameerkarale768@gmail.com>
2025-01-21 13:39:35 +0530

    Update CategoryController.cs

--- Machine_Test/Controllers/CategoryController.cs
+++ Machine_Test/Controllers/CategoryController.cs
@@ -12,84 +12,115 @@ namespace Machine_Test.Controllers
         {
             this.context = context;
         }
-  
-        public ActionResult Index()
+
+        // Index Action with Pagination
+        public ActionResult Index(int page = 1, int pageSize = 10)
         {
-            var categories = context.Categories.ToList();
+            var totalCategories = context.Categories.Count();
+            var categories = context.Categories
+                .OrderBy(c => c.Name)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)Math.Ceiling(totalCategories / (double)pageSize);
+
             return View(categories);
         }
 
-        public IActionResult DetailsCaterory(int id)
+        // View Details
+        public IActionResult DetailsCategory(int id)
         {
-            var categories = context.Categories.FirstOrDefault(x=>x.CategoryId== id);
-            if (categories == null)
-            {
-                return NotFound();  
-            }
-            return View(categories);
-        }
-        public IActionResult CreateCategory()
-        {
-            return View();
-
-        }
-        [HttpPost]
-        public IActionResult CreateCategory(Category category)
-        {
-            if (!ModelState.IsValid)
-            {
-
-                context.Categories.Add(category);
-                context.SaveChanges();
-               return RedirectToAction("Index");
-            }
-
-            return  View(category);
-
-        }
-        public IActionResult EditCategory(int id)
-        {
-            var category = context.Categories.FirstOrDefault(x=>x.CategoryId == id);
+            var category = context.Categories.FirstOrDefault(x => x.CategoryId == id);
             if (category == null)
             {
                 return NotFound();
             }
+            return View(category);
+        }
+
+        // Create Category (GET)
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        // Create Category (POST)
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            if (context.Categories.Any(c => c.Name == category.Name))
+            {
+                ModelState.AddModelError("Name", "Category with this name already exists.");
+                return View(category);
+            }
+
+            if (ModelState.IsValid)
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
             return View(category);
-
         }
+
+        // Edit Category (GET)
+        public IActionResult EditCategory(int id)
+        {
+            var category = context.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // Edit Category (POST)
         [HttpPost]
         public IActionResult EditCategory(Category category)
         {
-            if (!ModelState.IsValid)
+            if (context.Categories.Any(c => c.Name == category.Name && c.CategoryId != category.CategoryId))
+            {
+                ModelState.AddModelError("Name", "Category with this name already exists.");
+                return View(category);
+            }
+
+            if (ModelState.IsValid)
             {
                 context.Categories.Update(category);
                 context.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View(category);
         }
+
+        // Delete Category (GET)
         public IActionResult DeleteCategory(int id)
         {
-            var category=context.Categories.FirstOrDefault(x=>x.CategoryId==id);
+            var category = context.Categories.FirstOrDefault(x => x.CategoryId == id);
             if (category == null)
             {
                 return NotFound();
             }
             return View(category);
-
         }
-        [HttpPost ,ActionName("DeleteCategory")]
-        public IActionResult ConfirmDeleteCategory(Category category)
-        {
-            if (!ModelState.IsValid)
-            {
 
-                context.Categories.Remove(category);
-                context.SaveChanges();
-                return RedirectToAction("Index");
+        // Delete Category (POST)
+        [HttpPost, ActionName("DeleteCategory")]
+        public IActionResult ConfirmDeleteCategory(int id)
+        {
+            var category = context.Categories.FirstOrDefault(x => x.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
             }
-            return View(category);
+
+            context.Categories.Remove(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
